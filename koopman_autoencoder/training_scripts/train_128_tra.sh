382a9e92ea0d59cd30899cfb062b30debde80b18
rg625 <rg625@cam.ac.uk>
2025-05-19 15:33:16 +0100

    grouped scripts together

---
+++ koopman_autoencoder/training_scripts/train_128_tra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A script to train the Koopman Autoencoder model with the specified configuration
+
+# Define the path to the configuration file
+CONFIG_PATH=~/mnt/ocean_forecasting/koopman_autoencoder/configs/model/128_tra.yaml
+
+# Run the training script with the configuration file
+# python train.py --config "$CONFIG_PATH"
+python ./train.py --config "$CONFIG_PATH"
